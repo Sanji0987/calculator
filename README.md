f092e8850776e6a1bf79c8f11b52ae9674c5e51f
Sanjith Saravanan <sanjay200617@gmail.com>
2025-08-31 21:21:17 +0400

    added buttons finally after lazing off for a day

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # calculator
+
+Calculator app using gtk4 and c , mainly so that i learn gtk4 and some OOPS concepts
--- main.c
+++ main.c
@@ -2,13 +2,57 @@
 #include <gtk/gtk.h>
 
 
+
+#define buff_size 1000
+
+
+static void print_hello (GtkWidget *widget, gpointer data){
+  g_print ("Hello World\n");
+  char *datap = (char*) data;
+  datap[0] = 'c';
+  printf("datap : %c\n", datap[0]);
+}
+
+
 static void activate (GtkApplication *app, gpointer user_data){
 	GtkWidget *window;
-	
+	GtkWidget *button;
+	GtkWidget *grid;
+
+
+
 	window = gtk_application_window_new(app);
 	gtk_window_set_title (GTK_WINDOW (window), "Window");
-	gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+	gtk_window_set_default_size (GTK_WINDOW (window), 800, 800);
   	gtk_window_present (GTK_WINDOW (window));
+	
+	grid = gtk_grid_new ();
+	
+  	gtk_window_set_child (GTK_WINDOW (window), grid);
+	gtk_grid_set_row_spacing(GTK_GRID(grid), 1);   
+	gtk_grid_set_column_spacing(GTK_GRID(grid), 1);
+
+
+
+	GtkWidget *widget_array[10];
+
+	widget_array[0] = gtk_button_new_with_label("0");
+	g_signal_connect (widget_array[0], "clicked", G_CALLBACK (print_hello), user_data);
+	gtk_grid_attach(GTK_GRID(grid), widget_array[0] ,0 , 0 , 100 , 100 );
+
+	for (int i = 1 ; i <  10 ; i++){
+		widget_array[i] = gtk_button_new_with_label("1");
+  		g_signal_connect (widget_array[i], "clicked", G_CALLBACK (print_hello), user_data);
+		gtk_grid_attach_next_to(GTK_GRID(grid), widget_array[i], widget_array[i-1], GTK_POS_RIGHT , 100 , 100 );
+	}	
+
+
+
+	
+
+
+  	gtk_window_present (GTK_WINDOW (window));
+
 }
 
 
@@ -16,14 +60,15 @@ static void activate (GtkApplication *app, gpointer user_data){
 int main(int argc , char **argv){
 	GtkApplication *app;
 	int status;
-
+	
+	char *buff= (char*) calloc(sizeof(char), buff_size);
 
 	app = gtk_application_new ("org.sanjith.calculator", G_APPLICATION_DEFAULT_FLAGS);
-	g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+	g_signal_connect (app, "activate", G_CALLBACK (activate), buff);
 	status = g_application_run (G_APPLICATION (app), argc, argv);
 	g_object_unref (app);
-
 	
-
+		
+	free(buff);
 	return status;
 }
